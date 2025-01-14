fb44565fb609347f77019a03976ce6d889bd07c5
Hyeon Jong Kim <khje1027@gmail.com>
2025-01-14 22:12:26 +0900

    결과 페이지에 진단받기 버튼 추가

--- src/app/[type]/page.tsx
+++ src/app/[type]/page.tsx
@@ -1,11 +1,12 @@
 // app/[type]/page.tsx
 'use client';
-
+import Link from 'next/link'
 import { TypeInfo } from '../types';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import ShareAndReportButton from '@/components/ShareAndReportButton';
 import Image from 'next/image';
 import { useRouter, useParams } from 'next/navigation';
+import { Button } from '@/components/ui/button';
 
 export default function ResultPage() {
   const router = useRouter();
@@ -49,11 +50,20 @@ export default function ResultPage() {
                 height={40}
                 priority
               />
-          </div>
-          <div className="mt-8 flex flex-col items-center bg-gray-100 p-4 rounded-lg shadow-lg gap-2">
-            <h3 className='text-sm border-b-2 w-full text-center pb-2'>인스타그램에 보고서 사진(꾹 눌러서 다운로드하시면 됩니다!)을 사이트 링크와 함께 스토리 공유시 @yaktoon 태그해서 올리면 추첨을 통해 기프티콘을 보내드립니다!</h3>
-            <ShareAndReportButton type={type} />
-            {/* <a href="https://medsky.co.kr/coaching" target="_blank" rel="noopener noreferrer" className="w-full max-w-md">
+            </div>
+            <div className="mt-8 flex flex-col items-center bg-gray-100 p-4 rounded-lg shadow-lg gap-2">
+              <h3 className='text-sm border-b-2 w-full text-center pb-2'>인스타그램에 보고서 사진(꾹 눌러서 다운로드하시면 됩니다!)을 사이트 링크와 함께 스토리 공유시 @yaktoon 태그해서 올리면 추첨을 통해 기프티콘을 보내드립니다!</h3>
+              <ShareAndReportButton type={type} />
+
+              <Button className="w-full bg-gray-500 hover:bg-black text-white">
+                <Link
+                  href={"/"}
+                  className="w-full"
+                >
+                  진단 받기
+                </Link>
+              </Button>
+              {/* <a href="https://medsky.co.kr/coaching" target="_blank" rel="noopener noreferrer" className="w-full max-w-md">
                 <Button variant="default" className="w-full bg-black text-white">
                   학습 코칭 알아보기
                 </Button>
@@ -63,10 +73,10 @@ export default function ResultPage() {
                   카카오톡 상담하기
                 </Button>
               </a> */}
+            </div>
           </div>
-        </div>
-      </CardContent>
-    </Card>
+        </CardContent>
+      </Card>
     </main >
   );
 }
